f4bc31aba297339e5e53fd3720a8afb2e6bc120e
Rohan <rohanspahwa@gmail.com>
2020-07-24 14:13:42 -0400

    Initial Commit

---
+++ Enkrypt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Enkrypt
+//
+//  Created by Rohan Pahwa on 7/24/20.
+//  Copyright © 2020 Spring. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
